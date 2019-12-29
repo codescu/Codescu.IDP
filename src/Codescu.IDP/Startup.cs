e8a9d9d0009df989373906024d94763dd4211b20
Mihai Dumitru <549272+mihaimyh@users.noreply.github.com>
2019-12-29 02:42:16 +0200

    Read identity, api and client resources from appsettings.

--- src/Codescu.IDP/Startup.cs
+++ src/Codescu.IDP/Startup.cs
@@ -60,13 +60,13 @@ namespace Codescu.IDP
                 .AddAspNetIdentity<ApplicationUser>();
 
             // in-memory, code config
-            builder.AddInMemoryIdentityResources(Config.Ids);
-            builder.AddInMemoryApiResources(Config.Apis);
+            //builder.AddInMemoryIdentityResources(Config.Ids);
+            //builder.AddInMemoryApiResources(Config.Apis);
             //builder.AddInMemoryClients(Config.Clients);
 
             // or in-memory, json config
-            //builder.AddInMemoryIdentityResources(Configuration.GetSection("IdentityResources"));
-            //builder.AddInMemoryApiResources(Configuration.GetSection("ApiResources"));
+            builder.AddInMemoryIdentityResources(Configuration.GetSection("IdentityResources"));
+            builder.AddInMemoryApiResources(Configuration.GetSection("ApiResources"));
             builder.AddInMemoryClients(Configuration.GetSection("clients"));
 
             // not recommended for production - you need to store your key material somewhere secure
